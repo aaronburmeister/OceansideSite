28ac3378a5460e6fd1767c41a3a656edc1c80e96
aaronburmeister <f34r91@gmail.com>
2020-04-13 18:50:46 -0600

    Changed nav items with submenues to buttons, changed css animation

---
+++ app/components/header.js
@@ -0,0 +1,48 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object'
+
+export default class HeaderComponent extends Component {
+    @tracked expand = document.documentElement.clientWidth < 675 ? false : true;
+    @tracked expand_appointments = false;
+    @tracked expand_services = false;
+    @tracked expand_about = false;
+
+    @action
+    expandMenu() {
+        this.expand = !this.expand
+    }
+
+    @action
+    expandAppointments() {
+        this.expand_appointments = !this.expand_appointments
+        if (this.expand_services) {
+            this.expand_services = !this.expand_services
+        }
+        if (this.expand_about) {
+            this.expand_about = !this.expand_about
+        }
+    }
+
+    @action
+    expandServices() {
+        this.expand_services = !this.expand_services
+        if (this.expand_appointments) {
+            this.expand_appointments = !this.expand_appointments
+        }
+        if (this.expand_about) {
+            this.expand_about = !this.expand_about
+        }
+    }
+
+    @action
+    expandAbout() {
+        this.expand_about = !this.expand_about
+        if (this.expand_appointments) {
+            this.expand_appointments = !this.expand_appointments
+        }
+        if (this.expand_services) {
+            this.expand_services = !this.expand_services
+        }
+    }
+}
